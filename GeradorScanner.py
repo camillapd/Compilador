bc2620a5f5740aad24ebbc73e1e6e8d9bd5a4da0
Camilla <camillap@id.uff.br>
2024-04-18 18:52:56 -0300

    conversao afne -> afn done

--- GeradorScanner.py
+++ GeradorScanner.py
@@ -1,20 +1,29 @@
-class AFN:
+class Automato:
     def __init__(self):
-        self.meu_afn = {}
+        self.automato = {}
         self.estado_inicial = None
         self.estados_finais = []
 
-        # o afn é um meu_afn
         # é um dicionário no qual a chave é o vértice e os valores são listas de vértices
         # o vértice é um estado (e.g S1) e as listas de vértices são as transições (e.g S1->S2,S3)
 
     def add_estado(self, estado):
-        if estado not in self.meu_afn:
-            self.meu_afn[estado] = []
+        if estado not in self.automato:
+            self.automato[estado] = []
 
     def add_transicao(self, estado1, estado2, valor):
-        self.meu_afn[estado1].append((estado2, valor))
+        self.automato[estado1].append((estado2, valor))
 
+def traverse_tree_recursive(node):
+    if not node:
+        return
+
+    print(node.value)  
+    
+    for child in node.children:
+        traverse_tree_recursive(child) 
+
+# ER -> AFNE
 def algoritmo_thompson(er):
     pilha = list(er)
     pilha_afn = []
@@ -24,41 +33,39 @@ def algoritmo_thompson(er):
         if pilha[i] == '.':
             afn2 = pilha_afn.pop()
             afn1 = pilha_afn.pop()
-            afn = AFN()
+            afn = Automato()
 
             # adiciona o estado inicial, vai ser sempre o estado inicial do afn mais a esquerda (o afn1)
             afn.estado_inicial = afn1.estado_inicial
 
-            # adiciona todos estados e transições do afn1 pro novo afn                    
-            for i in afn1.meu_afn:
-                estado = i
+            # adiciona todos estados e transições do afn1 pro novo afn
+            for estado in afn1.automato:
                 if estado in afn1.estados_finais:
                     afn.add_estado(estado)
                     # adiciona transição epsilon pro próximo afn
                     afn.add_transicao(estado, afn2.estado_inicial, 'e')
-                for j in range(len(afn1.meu_afn.get(estado))):
+                for j in range(len(afn1.automato.get(estado))):
                     afn.add_estado(estado)
-                    afn.add_transicao(estado, afn1.meu_afn.get(estado)[
-                                      j][0], afn1.meu_afn.get(estado)[j][1])                
+                    afn.add_transicao(estado, afn1.automato.get(estado)[
+                                      j][0], afn1.automato.get(estado)[j][1])
 
-            # adiciona todos estados e transições do afn2 pro novo afn          
-            for i in afn2.meu_afn:
-                estado = i
+            # adiciona todos estados e transições do afn2 pro novo afn
+            for estado in afn2.automato:
                 if estado in afn2.estados_finais:
                     # só copia o estado final
                     afn.add_estado(estado)
                     afn.estados_finais.append(estado)
-                for j in range(len(afn2.meu_afn.get(estado))):
+                for j in range(len(afn2.automato.get(estado))):
                     afn.add_estado(estado)
-                    afn.add_transicao(estado, afn2.meu_afn.get(estado)[
-                                      j][0], afn2.meu_afn.get(estado)[j][1])
+                    afn.add_transicao(estado, afn2.automato.get(estado)[
+                                      j][0], afn2.automato.get(estado)[j][1])
 
             pilha_afn.append(afn)
         elif pilha[i] == "*":
             afn1 = pilha_afn.pop()
-            afn = AFN()
+            afn = Automato()
 
-            estado_inicial = 'S' + str(count)
+            estado_inicial = 'E' + str(count)
 
             # cria novo estado inicial que também é final com transição epsilon pro afn
             afn.add_estado(estado_inicial)
@@ -67,18 +74,16 @@ def algoritmo_thompson(er):
             afn.add_transicao(estado_inicial, afn1.estado_inicial, 'e')
 
             # adiciona todos estados e transições do afn1 (velho) pro novo afn
-            for i in afn1.meu_afn:
-                estado = i
+            for estado in afn1.automato:
                 if estado in afn1.estados_finais:
                     afn.add_estado(estado)
                     # adiciona transição epsilon do estado final pro estado inicial
                     afn.add_transicao(estado, afn1.estado_inicial, 'e'),
                     afn.estados_finais.append(estado)
-                for j in range(len(afn1.meu_afn.get(estado))):
-
+                for j in range(len(afn1.automato.get(estado))):
                     afn.add_estado(estado)
-                    afn.add_transicao(estado, afn1.meu_afn.get(estado)[
-                                      j][0], afn1.meu_afn.get(estado)[j][1])
+                    afn.add_transicao(estado, afn1.automato.get(estado)[
+                                      j][0], afn1.automato.get(estado)[j][1])
 
             pilha_afn.append(afn)
         elif pilha[i] == "+":
@@ -86,10 +91,10 @@ def algoritmo_thompson(er):
         elif pilha[i] == "|":
             afn2 = pilha_afn.pop()
             afn1 = pilha_afn.pop()
-            afn = AFN()
+            afn = Automato()
 
-            estado_inicial = 'S' + str(count)
-            estado_final = 'S' + str(count+1)
+            estado_inicial = 'U' + str(count)
+            estado_final = 'U' + str(count+1)
 
             # adiciona novo estado inicial
             afn.add_estado(estado_inicial)
@@ -97,29 +102,27 @@ def algoritmo_thompson(er):
 
             # adiciona transições epsilon do novo estado inicial pros antigos estados inicias dos 2 afns
             afn.add_transicao(estado_inicial, afn1.estado_inicial, 'e')
-            afn.add_transicao(estado_inicial, afn2.estado_inicial, 'e')          
-            
+            afn.add_transicao(estado_inicial, afn2.estado_inicial, 'e')
+
             # adiciona todos estados e transições do afn1 pro novo afn
-            for i in afn1.meu_afn:
-                estado = i
+            for estado in afn1.automato:
                 if estado in afn1.estados_finais:
                     afn.add_estado(estado)
                     afn.add_transicao(estado, estado_final, 'e')
-                for j in range(len(afn1.meu_afn.get(estado))):
+                for j in range(len(afn1.automato.get(estado))):
                     afn.add_estado(estado)
-                    afn.add_transicao(estado, afn1.meu_afn.get(estado)[
-                                      j][0], afn1.meu_afn.get(estado)[j][1])
-                             
-            # adiciona todos estados e transições do afn2 pro novo afn          
-            for i in afn2.meu_afn:
-                estado = i
+                    afn.add_transicao(estado, afn1.automato.get(estado)[
+                                      j][0], afn1.automato.get(estado)[j][1])
+
+            # adiciona todos estados e transições do afn2 pro novo afn
+            for estado in afn2.automato:
                 if estado in afn2.estados_finais:
                     afn.add_estado(estado)
                     afn.add_transicao(estado, estado_final, 'e')
-                for j in range(len(afn2.meu_afn.get(estado))):
+                for j in range(len(afn2.automato.get(estado))):
                     afn.add_estado(estado)
-                    afn.add_transicao(estado, afn2.meu_afn.get(estado)[
-                                      j][0], afn2.meu_afn.get(estado)[j][1])
+                    afn.add_transicao(estado, afn2.automato.get(estado)[
+                                      j][0], afn2.automato.get(estado)[j][1])
 
             # adiciona o novo estado final
             afn.add_estado(estado_final)
@@ -128,9 +131,9 @@ def algoritmo_thompson(er):
             pilha_afn.append(afn)
         else:  # caso é uma letra do alfabeto
             simbolo = pilha[i]
-            afn = AFN()
-            estado_inicial = 'S' + str(count)
-            estado_final = 'S' + str(count+1)
+            afn = Automato()
+            estado_inicial = simbolo.upper() + str(count)
+            estado_final = simbolo.upper() + str(count+1)
 
             afn.add_estado(estado_inicial)
             afn.estado_inicial = estado_inicial
@@ -146,8 +149,96 @@ def algoritmo_thompson(er):
 
     return pilha_afn[0]
 
-meu_novo_afn = algoritmo_thompson("ab|*a.b.")
-print(meu_novo_afn.meu_afn)
+# AFNE -> AFN -> AFD
+def construcao_subconjuntos(afne):
+    pilha = []
+    pilha.append(afne)
+    afd = Automato()
+
+    # (1) calcula o fecho epsilon #
+    at_fecho = Automato()
+
+    # copia o afne pro automato com fecho epsilon (at_fecho)
+    for estado in afne.automato:
+        if estado == afne.estado.inicial:
+            at_fecho.add_estado(estado)
+            at_fecho.estado_inicial = estado
+        if estado in afne.estados_finais:
+            at_fecho.add_estado(estado)
+            at_fecho.estado_final.append(estado)
+        for j in range(len(afne.automato.get(estado))):
+            at_fecho.add_estado(estado)
+            at_fecho.add_transicao(estado, afne.automato.get(estado)[
+                j][0], afne.automato.get(estado)[j][1])
+
+    # adiciona fecho epsilon no at_fecho TODO
+    for estado in afne.automato:
+        for j in range(len(afne.automato.get(estado))):
+            if afne.automato.get(estado)[j][1] == 'e':
+                res =  afne.automato.get(estado)[j][0]
+                # recursivamente pegar fecho (e) de res e somar ao estado embaixo
+                # at_fecho.add_transicao(estado, estado + coisas, 'fecho (e)')
+            else:
+                at_fecho.add_transicao(estado, estado, 'fecho (e)')
+                
+    # remove todas as transições 'e' do at_fecho TODO
+                
+    # (2) converte afne para afn #
+    afn = Automato()
+    
+    for estado in at_fecho.automato:
+        afn.add_estado(estado)
+        if estado == at_fecho.estado_inicial:
+            afn.add_estado(at_fecho.automato.get(estado)[-1][0])
+            afn.estado_inicial = at_fecho.automato.get(estado)[-1][0]
+        if estado in at_fecho.estados_finais:
+            afn.add_estado(estado)
+            afn.estados_finais.append(estado)
+        else:
+            afn.add_estado(estado)
+
+        # o valor do fecho do estado atual
+        fecho_estado = at_fecho.automato.get(estado)[-1][0]
+        # o símbolo da setinha que vou colocar para o novo estado do afn
+        fecho_res = ""
+
+        for j in range(len(at_fecho.automato.get(estado))-1):
+
+            # checo se o valor do fecho é uma lista de estados
+            # se for eu coloco os valores em uma lista e itero por eles
+            if "," in fecho_estado:
+                list_fecho = fecho_estado.split(",")
+                for fecho in list_fecho:
+
+                    # aqui eu checo se tem mais de uma transição nesse estado
+                    # porque se tiver uma só é o fecho e não quero contar isso nas minhas setinhas
+                    if len(at_fecho.automato.get(fecho)) > 1:
+
+                        # o destino é o valor da transição, para eu usar o fecho desse estado
+                        fecho_destino = at_fecho.automato.get(fecho)[j][0]
+                        um_fecho = at_fecho.automato.get(
+                            fecho_destino)[-1][0]
+
+                        # checagem para não colocar valores repetidos na transição nova
+                        if um_fecho not in fecho_res:
+                            # como tem mais de um estado em que estou olhando o fecho, aqui junto todos
+                            fecho_res = um_fecho + fecho_res
+            else:
+                # aqui eu checo se tem mais de uma transição nesse estado
+                # porque se tiver uma só é o fecho e não quero contar isso nas minhas setinhas
+                if len(at_fecho.automato.get(fecho_estado)) > 1:
+                    fecho_destino = at_fecho.automato.get(fecho_estado)[j][0]
+                    fecho_res = at_fecho.automato.get(fecho_destino)[-1][0]
+
+            afn.add_transicao(estado, fecho_res,
+                            at_fecho.automato.get(estado)[j][1])                           
+
+    # (3) converte afn para afd # TODO
+
+    return afd
+
+meu_novo_afn = algoritmo_thompson("ab|*a.")
+print(meu_novo_afn.automato)
 print(meu_novo_afn.estado_inicial)
 print(meu_novo_afn.estados_finais)
 
--- teste.py
+++ teste.py
@@ -1,4 +1,79 @@
-grafo = {'SA': [('S2', 'a'), ('S3', 'b'), ('S4', 'b')], 'SB': [('S5', 'a')]}
+grafo = {'S1': [('S2', 'a'), ('S1', 'fecho')],
+         'S2': [('S6', 'e'), ('S1,S2', 'fecho')]}
+
+
+class Automato:
+    def __init__(self):
+        self.automato = {}
+        self.estado_inicial = None
+        self.estados_finais = []
+
+    def add_estado(self, estado):
+        if estado not in self.automato:
+            self.automato[estado] = []
+
+    def add_transicao(self, estado1, estado2, valor):
+        self.automato[estado1].append((estado2, valor))
+
+
+at_fecho = Automato()
+at_fecho.estado_inicial = 'S1'
+at_fecho.estados_finais.append('S3')
+at_fecho.automato = {'S1': [('S2', 'a'), ('S1', 'fecho')],
+                     'S2': [('S3', 'a'), ('S1,S2,S3', 'fecho')],
+                     'S3': [('S3', 'fecho')]}
+
+afn = Automato()
+
+for estado in at_fecho.automato:
+    afn.add_estado(estado)
+    if estado == at_fecho.estado_inicial:
+        afn.add_estado(at_fecho.automato.get(estado)[-1][0])
+        afn.estado_inicial = at_fecho.automato.get(estado)[-1][0]
+    if estado in at_fecho.estados_finais:
+        afn.add_estado(estado)
+        afn.estados_finais.append(estado)
+    else:
+        afn.add_estado(estado)
+
+    # o valor do fecho do estado atual
+    fecho_estado = at_fecho.automato.get(estado)[-1][0]
+    # o símbolo da setinha que vou colocar para o novo estado do afn
+    fecho_res = ""
+
+    for j in range(len(at_fecho.automato.get(estado))-1):
+
+        # checo se o valor do fecho é uma lista de estados
+        # se for eu coloco os valores em uma lista e itero por eles
+        if "," in fecho_estado:
+            list_fecho = fecho_estado.split(",")
+            for fecho in list_fecho:
+
+                # aqui eu checo se tem mais de uma transição nesse estado
+                # porque se tiver uma só é o fecho e não quero contar isso nas minhas setinhas
+                if len(at_fecho.automato.get(fecho)) > 1:
+
+                    # o destino é o valor da transição, para eu usar o fecho desse estado
+                    fecho_destino = at_fecho.automato.get(fecho)[j][0]
+                    um_fecho = at_fecho.automato.get(
+                        fecho_destino)[-1][0]
+
+                    # checagem para não colocar valores repetidos na transição nova
+                    if um_fecho not in fecho_res:
+                        # como tem mais de um estado em que estou olhando o fecho, aqui junto todos
+                        fecho_res = um_fecho + fecho_res
+        else:
+            # aqui eu checo se tem mais de uma transição nesse estado
+            # porque se tiver uma só é o fecho e não quero contar isso nas minhas setinhas
+            if len(at_fecho.automato.get(fecho_estado)) > 1:
+                fecho_destino = at_fecho.automato.get(fecho_estado)[j][0]
+                fecho_res = at_fecho.automato.get(fecho_destino)[-1][0]
+
+        afn.add_transicao(estado, fecho_res,
+                          at_fecho.automato.get(estado)[j][1])
+
+print(afn.automato)
+
 
 # print(grafo)
 
@@ -15,16 +90,25 @@ grafo = {'SA': [('S2', 'a'), ('S3', 'b'), ('S4', 'b')], 'SB': [('S5', 'a')]}
 #             # print(list(grafo.values())[i][j][0], 'i,j,0')
 #             # print(list(grafo.values())[i][j][1], 'i,j,1')  # o valor
 
-for k in grafo:
-    estado = k
-    print(estado)
-    for i in range(len(grafo.get(estado))):
-        print(grafo.get(estado)[i], 'i')
-        print(grafo.get(estado)[i][0],'j 0')
-        print(grafo.get(estado)[i][1], 'j 1')
+# for k in grafo:
+#     estado = k
+#     print(estado)
+#     for i in range(len(grafo.get(estado))):
+#         print(grafo.get(estado)[i], 'i')
+#         print(grafo.get(estado)[i][0],'j 0')
+#         print(grafo.get(estado)[i][1], 'j 1')
 
 x = grafo.get("SA")
-print(grafo.get("SA")[0][0])
+# print(grafo.get("SA")[0][0])
+
+
+
+
+
+
+
+
+
 
 # TODO
 # for i in grafo:
